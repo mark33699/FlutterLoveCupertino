c8f1411e444fb9c278772cb84438f849341fe19a
angelmark <mark.hsieh@angelsctek.com>
2020-10-19 11:32:30 +0800

    換成MaterialApp有正常一點了啦...但還是會跑版

--- cupertino/lib/main.dart
+++ cupertino/lib/main.dart
@@ -2,13 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 void main() {
-  runApp(AppleApp());
+//  runApp(AppleApp());
+  runApp(AndroidApp());
 }
+
+final List<String> _tonightOption = [
+  "鼎泰豐的小籠包佐薑絲米醋",
+  "金峰的滷肉飯加蛋",
+  "可不可的白玉歐蕾去冰",
+  "林東芳的半筋半肉牛肉麵",
+  "麥當勞的勁辣雞腿堡配薯條可樂",
+  "珍煮丹的黑糖珍珠厚鮮奶半糖少冰",
+  "星巴克的特選馥郁那提配起司牛肉可頌",
+  "八方雲集的韓式辣味鍋貼配酸辣湯",
+  "永和豆漿的爆漿蔥油餅配熱米漿",
+  "王品的經典牛小排配奶油菠菜",
+  "鼎泰豐的小籠包佐薑絲米醋",
+];
+
+//- Apple
 
 class AppleApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return CupertinoApp(
       debugShowCheckedModeBanner: false,
       title: 'Cupertino Demo',
@@ -29,19 +47,6 @@ class CupertinoHomePage extends StatefulWidget {
 class _CupertinoHomePageState extends State<CupertinoHomePage> {
 
   String _tonightSelected = "";
-  List<String> _tonightOption = [
-    "鼎泰豐的小籠包佐薑絲米醋",
-    "金峰的滷肉飯加蛋",
-    "可不可的白玉歐蕾去冰",
-    "林東芳的半筋半肉牛肉麵",
-    "麥當勞的勁辣雞腿堡配薯條可樂",
-    "珍煮丹的黑糖珍珠厚鮮奶半糖少冰",
-    "星巴克的特選馥郁那提配起司牛肉可頌",
-    "八方雲集的韓式辣味鍋貼配酸辣湯",
-    "永和豆漿的爆漿蔥油餅配熱米漿",
-    "王品的經典牛小排配奶油菠菜",
-    "鼎泰豐的小籠包佐薑絲米醋",
-  ];
 
   void _updateTonightSelected(int index){
     setState(() {
@@ -79,6 +84,72 @@ class _CupertinoHomePageState extends State<CupertinoHomePage> {
           ],
         ),
       )
+    );
+  }
+}
+
+//- Android
+
+class AndroidApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material Demo',
+      theme: ThemeData(primaryColor: Colors.deepOrange),
+      home: AndroidHomePage(title: 'Material Demo Home Page'),
+    );
+  }
+}
+
+class AndroidHomePage extends StatefulWidget {
+  AndroidHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _AndroidHomePageState createState() => _AndroidHomePageState();
+}
+
+class _AndroidHomePageState extends State<AndroidHomePage> {
+
+  String _tonightSelected = "";
+
+  void _updateTonightSelected(int index){
+    setState(() {
+      _tonightSelected = _tonightOption[index];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Material")),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                  child: Center(
+                    child: Text("今晚 你想來點：\n$_tonightSelected",
+                      textAlign: TextAlign.center,),
+                  )
+              ),
+
+              Container(
+                color: Colors.orange,
+                height: 200,
+                child: CupertinoPicker(
+                  onSelectedItemChanged: _updateTonightSelected,
+                  itemExtent: 40,
+                  children: List.generate(_tonightOption.length, (index) {
+                    return Text(_tonightOption[index]);
+                  }),
+                ),
+              )
+            ],
+          ),
+        )
     );
   }
 }
